390199c739beff131cfd04797e6163b9bd42bcd0
BOXGAME <wwwboxgamepl@gmail.com>
2016-09-09 03:07:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -42,10 +42,17 @@ END;
    }(document, 'script', 'facebook-jssdk'));
 </script>
 <br>
-<img= "http://devtut.pl/gameimg/promo_fb_eater.png"</img>
+<div class="fb-login-button" data-max-rows="100" data-size="xlarge" data-show-faces="false" data-auto-logout-link="false"></div>
 <img src="http://devtut.pl/gameimg/promo_fb_eater.png" alt="FB EATER">
 <br>
 <center><h1>COMMING SOON (2016) TO FB, ANDROID, AND WEB_GL.</h1></center>
-
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v2.7&appId=570619062966909";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 </body>
 </html>
